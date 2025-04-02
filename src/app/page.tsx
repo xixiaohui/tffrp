880f9edb21a540e884f38b9c02b32aeef6037f13
frank <yamazake@yeah.com>
2025-04-02 03:16:06 +0100

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,29 +6,25 @@ export default function Home() {
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
-          src="/next.svg"
+          src="/icon.jpg"
           alt="Next.js logo"
-          width={180}
-          height={38}
+          width={80}
+          height={80}
           priority
         />
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
+            Fiberglass Raw Materials Distributor.
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
+            One-stop supplier of composite materials.
           </li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            href="mailto:info@tffrp.com"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -39,22 +35,22 @@ export default function Home() {
               width={20}
               height={20}
             />
-            Deploy now
+            Contact Us
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            href="mailto:info@tffrp.com"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docs
+            info@tffrp.com
           </a>
         </div>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href=""
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -65,11 +61,11 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Learn
+          Fiberglass Roving
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href=""
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -80,11 +76,11 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Examples
+          Fiberglass Yarns
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://www.tfcomposite.com"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -95,7 +91,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to nextjs.org →
+          Go to tfcomposite.com →
         </a>
       </footer>
     </div>
